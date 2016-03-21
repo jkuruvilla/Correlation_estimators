003bd603f1a26a1e9c8032383c0ac365531534cd
Joseph Kuruvilla <joseph@astro.uni-bonn.de>
2016-03-21 11:27:01 +0100

    first code commit

---
+++ Correlation_numbercount.py
@@ -0,0 +1,47 @@
+__license__   = "GNU GPLv3 <https://www.gnu.org/licenses/gpl.txt>"
+__copyright__ = "2016, Joseph Kuruvilla"
+__author__    = "Joseph Kuruvilla <joseph.k@uni-bonn.de>"
+__version__   = "1.0"
+
+'''
+Program to determine the correlation function using Landy-Szalay estimator
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+# ------------------
+# Importing Modules
+# ------------------
+
+import numpy as np
+import h5py
+from ConfigParser import SafeConfigParser
+
+# -------------------
+# Config file reader
+# -------------------
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+
+# --------------------
+#   Program  start
+# --------------------
+
+if __name__ == "__main__":
+
+  input_file = parser.get('input','file')
+  snap = h5py.File(input_file ,'r')
+
+  pos = snap["/PartType1/Coordinates"][:]
